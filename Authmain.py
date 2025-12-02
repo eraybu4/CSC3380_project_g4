75fa93f81687d0f5de1465d8dc380c294978f63d
nquint8 <108764819+nquint8@users.noreply.github.com>
2025-12-02 04:55:46 -0600

    Update Authmain.py

--- Authmain.py
+++ Authmain.py
@@ -1,5 +1,5 @@
 from _typeshed import IdentityFunction
-from flask import Flask,render_template,request,redirect, session
+from flask import Flask,render_template,request,redirect, session, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
@@ -18,7 +18,7 @@ class User(db.Model):
     password_hash= db.Column(db.String(150), nullable=False)
 
     def set_password(self, password):
-        self.password= generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
         
     def check_password(self, password):
         return check_password_hash(self.password_hash,password)
@@ -28,34 +28,43 @@ class User(db.Model):
 @app.route("/")
 def home():
     if "username" in session:
-        return redirect(url_for("/dashboard"))
+        return redirect(url_for("dashboard"))
     return render_template('index.html')
 
 #Login route
 @app.route("/login", methods=["POST"])
 def login():
-        if request.method=="POST":
-            username=request.form.get("username")
-            password=request.form.get("password")
-            user=User.query.filter_by(username=username).first()
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        
         if user and user.check_password(password):
-            session["username"]=username
+            session["username"] = username
             return redirect(url_for("dashboard"))
         else:
             return "Invalid credentials"
-        return render_template("Index.html")
+    
+    return render_template("index.html")
 
 #register route
 @app.route("/register", methods=["POST"])
 def register():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    
+    user = User.query.filter_by(username=username).first()
+    
     if user:
         return render_template("index.html", error="User already exists")
     else:
-        new_user=User(username=username)
+        new_user = User(username=username)
         new_user.set_password(password)
+        db.session.add(new_user)
         db.session.commit()
-        session["username"]=username
+        session["username"] = username
         return redirect(url_for("dashboard"))
+
 #Dashboard route
 @app.route("/dashboard")
 def dashboard():
@@ -75,4 +84,3 @@ if __name__ in '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
