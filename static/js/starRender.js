887d30900db13da49b115fc97f4f4ae2309dfe99
Ndukes04 <136605848+Ndukes04@users.noreply.github.com>
2025-12-10 00:24:47 -0600

    Update starRender.js
    Documentation!

--- static/js/starRender.js
+++ static/js/starRender.js
@@ -1,3 +1,7 @@
+/**
+* starRender.js
+* Renders the constellations in all of the inner boxes. 
+*/
 
 	const boxes = document.querySelectorAll('.box1_inner, .box2_inner, .box3_inner');
 
@@ -6,7 +10,10 @@
 		const svg = box.querySelector('svg');
 		const stars = Array.from(box.querySelectorAll('.star'));
 
-
+	/**
+	* Creates and appends the lines to the SVG. 
+	* Wipes any pre-existing elements before appending any new ones. 
+	*/
 		 function createLines() {
 			while (svg.firstChild) svg.removeChild(svg.firstChild);
 			const needed = stars.length - 1
@@ -18,6 +25,10 @@
     		}
   		 }
 
+	/**
+	* Gets the position and size of the element and SVG.
+	* Uses them to calculate the center of the star PNG. 
+	*/
 		 function centerOf(element) {
 			const svgRect = svg.getBoundingClientRect();
 			const r = element.getBoundingClientRect();
@@ -27,6 +38,10 @@
 			};
 		 }
 
+	/**
+	* Renders the connecting lines using the center coordinates of two stars at a time.
+	* Sets those center coordinates as the start and endpoint of the line.
+	*/
 		 function updateLines() {
 			const lines = Array.from(svg.querySelectorAll('line'));
 			for (let i = 0; i < lines.length; i++) {
@@ -44,5 +59,6 @@
 			updateLines();
 
 			window.addEventListener('resize', updateLines);
+
 
 	});
